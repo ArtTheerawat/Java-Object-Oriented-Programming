a9b5ac6c8ad724d994323bec1f35b161fadf680b
unknown <theerawat.numtang@gmail.com>
2026-03-13 22:49:38 +0700

    Ex1

--- Chapter1/Ex1.java
+++ Chapter1/Ex1.java
@@ -1,5 +1,7 @@
 public class Ex1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is fun.");
     }
 }
